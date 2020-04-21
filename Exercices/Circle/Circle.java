91b0af39e671116f45703dfdc111d37f0e994599
CocoCouq <couq@icloud.com>
2020-04-22 01:22:41 +0200

    Beginnings

---
+++ Exercices/Circle/Circle.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Circle
+{
+    final static double PI=3.1416;
+
+    // Rayon
+    public static double rayon()
+    {
+        System.out.println("Entrer le rayon: ");
+        Scanner readEntry = new Scanner(System.in);
+
+        return readEntry.nextFloat() ;
+    }
+
+    // Circonférence
+    public static double circon(double rayon)
+    {
+        return 2 * PI * rayon;
+    }
+
+    // Surface
+    public static double surface(double rayon)
+    {
+        return PI * Math.pow(rayon, 2);
+    }
+
+    // Affichage
+    public static void affiche(double circ, double surf)
+    {
+        // Affichage
+        System.out.print("La circonférence est de: ");
+        System.out.println(circ);
+        System.out.print("La surface est de: ");
+        System.out.println(surf);
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+    // MAIN
+    public static void main(String[] args)
+    {
+        System.out.println("**** Circonférence et surface du Cercle (V1.0 - 04/20) ****");
+
+        // Rayon
+        double rayon = rayon();
+
+        // Perimetre
+        double circ = circon(rayon);
+
+        // Surface
+        double surf = surface(rayon);
+
+        // Affichage
+        affiche(circ, surf);
+
+    }
+}
---
+++ Exercices/SwitchValue/SwitchValue.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SwitchValue
+{
+    // Scanner
+    final static Scanner READ = new Scanner(System.in);
+
+    // Define one number
+    public static double defineNumber(String message)
+    {
+        System.out.println(message);
+        double number = READ.nextFloat();
+        return number;
+    }
+
+    // Display number
+    public static void displayNumber(double number, String message)
+    {
+        System.out.print(message);
+        System.out.println(number);
+    }
+
+    // MAIN
+    public static void main(String[]args)
+    {
+        System.out.println("**** Swap (V1.0 - 04/20) ****");
+
+        double nY = defineNumber("nY: ");
+        double nX = defineNumber("nX: ");
+
+        // swap values
+        double temp = nY;
+        nY = nX;
+        nX = temp;
+
+        displayNumber(nY, "nY: ");
+        displayNumber(nX, "nX: ");
+    }
+
+}
