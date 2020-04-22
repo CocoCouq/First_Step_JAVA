b395d367bba010338a29a20d1e57d96a9dd52128
CocoCouq <couq@icloud.com>
2020-04-22 09:42:02 +0200

    Ex 3

---
+++ Exercices/Operateurs/Fahrenheit.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Fahrenheit
+{
+    final static Scanner READ = new Scanner(System.in);
+
+    // Convert in Fahrenheit
+    public static double convertFahr(double fahr)
+    {
+        return ((fahr - 32) * 5) / 9;
+    }
+
+    // MAIN
+    public static void main(String[] args) {
+        System.out.println("**** Fahrenheit (V1.0 - 04/20) ****");
+
+        // Read Fahrenheit
+        System.out.println("Quelle est la température Fahrenheit ?");
+        double fahr = READ.nextDouble();
+
+        // Convert in Celsius
+        double celsius = convertFahr(fahr);
+
+        System.out.println("En Celsius: ");
+        System.out.println(celsius);
+    }
+}
