cf6e349058f356087016ba3df3d7b9fd224cb433
CocoCouq <couq@icloud.com>
2020-04-22 11:49:32 +0200

    Ex 1

---
+++ Exercices/Conditions/EvenNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class EvenNumber {
+    final static Scanner READ = new Scanner(System.in);
+
+    // MAIN
+    public static void main(String[] args) {
+        System.out.println("**** Parité (V1.0 - 04/20) ****");
+
+        System.out.println("Entrez un nombre :");
+        long number = READ.nextLong();
+
+        // If even or not
+        String result = number % 2 == 0 ? "Pair" : "Impair";
+
+        System.out.println(result);
+    }
+}
