5b04ee0d5657446f51923d3df6ef14b795c7e87f
CocoCouq <couq@icloud.com>
2020-04-24 12:56:25 +0200

    Java Project

--- FirstJavaProject/src/com/FirstJavaProject/Operators/Average.java
+++ FirstJavaProject/src/com/FirstJavaProject/Operators/Average.java
@@ -4,14 +4,14 @@ import com.FirstJavaProject.Lib;
 
 public class Average {
     // Average
-    public static double average(double[] tab) {
+    public static double average(double[] tab,int coe) {
         double sum = 0;
         int i = -1;
 
         while (++i < tab.length)
             sum += tab[i];
 
-        return sum / tab.length;
+        return sum / (tab.length + (coe - tab.length));
     }
 
     /* EXECUTE */
@@ -21,10 +21,10 @@ public class Average {
         // Notes
         double[] notes = new double[3];
         notes[0] = Lib.readDouble("Resultat du devoir à la Maison :");
-        notes[1] = Lib.readDouble("Resultat du devoir écrit :");
-        notes[2] = Lib.readDouble("Resultat du devoir oral :");
+        notes[1] = Lib.readDouble("Resultat du devoir écrit :") * 3;
+        notes[2] = Lib.readDouble("Resultat du devoir oral :") * 2;
 
         // Result
-        System.out.printf("Moyenne de %f", average(notes));
+        System.out.printf("Moyenne de %f", average(notes, 6));
     }
 }
